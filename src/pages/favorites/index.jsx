3b9511a1a760445286a02c2a6616317c956c8746
ErickWillyan <141357058+ErickWillyan@users.noreply.github.com>
2024-04-23 22:28:59 -0300

    feature removeItem

--- src/pages/favorites/index.jsx
+++ src/pages/favorites/index.jsx
@@ -1,20 +1,29 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./index.css";
 
 export default function Favorites() {
   const [movies, setMovies] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const response = localStorage.getItem("@favoritesMovies");
     setMovies(JSON.parse(response) || []);
   }, []);
 
-  function removeMovie() {
-    alert("Remover filme");
+  function removeMovie(id) {
+    let newList = movies.filter((item) => {
+      return item.id !== id;
+    });
+
+    setMovies(newList);
+    localStorage.setItem("@favoritesMovies", JSON.stringify(newList));
+
+    alert(`${id} deletado`);
   }
 
-  function navigateToDetailMovie() {
-    alert("detalhes do filme");
+  function navigateToDetailMovie(id) {
+    navigate(`/movie/${id}`);
   }
 
   if (movies.length === 0) {
@@ -33,10 +42,12 @@ export default function Favorites() {
         {movies.map((item) => {
           return (
             <div className="movieItem" key={item.id}>
-              <li onClick={navigateToDetailMovie}>
+              <li onClick={() => navigateToDetailMovie(item.id)}>
                 <p>{item.title}</p>
               </li>
-              <button onClick={removeMovie}>remover Filme</button>
+              <button onClick={() => removeMovie(item.id)}>
+                remover Filme
+              </button>
             </div>
           );
         })}
